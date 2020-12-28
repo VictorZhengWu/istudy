9e73f7114c56d777d1241c67c2823c06a45fdc33
VictorZhengWu <victor.zhengwu@outlook.com>
2020-12-28 22:53:07 +0800

    new

--- python/hellopython.py
+++ python/hellopython.py
@@ -1,2 +1,10 @@
-print("Hello Python!")
-print("Hello Xmas")
+################################
+name = input('Please input your name: ')
+age = input('Please input your age: ')
+job = input('Please input your job: ')
+
+template = '''Hello, my name is %s, 
+i am %s this year, 
+my job is %s''' %(name, age, job)
+
+print(template)
