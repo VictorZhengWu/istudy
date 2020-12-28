7f486e9a137ba12f35f3eeb6439a6e10615d5db4
VictorZhengWu <victor.zhengwu@outlook.com>
2020-12-28 22:54:02 +0800

    new name

---
+++ python/Study.py
@@ -0,0 +1,10 @@
+################################
+name = input('Please input your name: ')
+age = input('Please input your age: ')
+job = input('Please input your job: ')
+
+template = '''Hello, my name is %s, 
+i am %s this year, 
+my job is %s''' %(name, age, job)
+
+print(template)
