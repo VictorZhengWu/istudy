2ee7201f69b8ff2583fe858f5a6451391d8a98ce
VictorZhengWu <victor.zhengwu@outlook.com>
2020-12-29 21:16:28 +0800

    bit_length

--- python/Study.py
+++ python/Study.py
@@ -1,25 +1,17 @@
-<<<<<<< HEAD
-# ################################################################
-# ## 模板
-# name = input('Please input your name: ')
-# age = input('Please input your age: ')
-# job = input('Please input your job: ')
-=======
 ################################################################
 ## 模板
 name = input('Please input your name: ')
 age = input('Please input your age: ')
 job = input('Please input your job: ')
->>>>>>> 7e3aab69701ca743ab2e10718b43674535634aa8
 
-# template = '''Hello, my name is %s, 
-# i am %s this year, 
-# my job is %s''' %(name, age, job)
+template = '''Hello, my name is %s, 
+i am %s this year, 
+my job is %s''' %(name, age, job)
 
-# print(template)
+print(template)
 
 
 ################################################################
-## 模板
+## bit_length
 num = 10
 print(num.bit_length())
